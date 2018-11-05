3891fe7952fff9bfeb7dba028fae526c25b065cb
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-05 22:57:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,11 @@
-
-{
-  "name": "Emoko",
-  "description": "Emoko",
-  "version": "0.0.0",
-  "main": "bot.js",
-  "scripts": {
-    "start": "node bot.js"
-  },
-  "dependencies": {
-   "discord.js": "^11.3.2",
-    "fs": "^0.0.2",
-    "giphy-api": "1.2.7",
-    "math-expression-evaluator":"1.2.17",
-    "common-tags":"1.7.2",
-    "moment":"2.22.2",
-     "canvas":"1.6.11",
-    "jimp":"0.3.5",
-    "hastebin-gen":"1.3.1",
-    "google-translate-api":"2.3.0",
-    "ascii-data-table":"2.1.1"
-  }
-}
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '#'
+const prefix = '.'
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`.`,"http://twitch.tv/S-F")
+client.user.setGame(`Mody`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -51,6 +28,19 @@ client.user.setGame(`.`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+
+
+
+
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply("Yes.?");
+    }
+});
+
 
 
 
