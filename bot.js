123c9b18eb585e0b39bbfea7951d133cae12ac05
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-06 16:08:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,41 +97,39 @@ client.on ('message', msg => {
 
 
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith('.ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms")
+.addField('**WebSocket:**',api + " ms")
+message.channel.send({embed:embed});
+}
+});
+
+
+
+
+
+
+
 
 client.on('message', message => {
-    if (message.content === '.user') {
-        if(!message.channel.guild) return message.reply(`❌ هذا الأمر فقط ل السيرفرات`);
-   
-   message.guild.fetchInvites().then(invs => {
-   let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-   let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-   var moment = require('moment');
-   var men = message.mentions.users.first();
-   var heg;
-   if(men) {
-   heg = men
-   } else {
-   heg = message.author
-   }
-   var mentionned = message.mentions.members.first();
-   var h;
-   if(mentionned) {  /// tm t3del mn Fox
-   h = mentionned
-   } else {
-   h = message.member
-   }
-   moment.locale('ar-TN');
-   var idDark = new  Discord.RichEmbed()
-   
-   .setColor("RANDOM")
-     .setThumbnail(message.author.avatarURL)
-   .addField(' دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('`D/M/YYYY HH:mm`')} **\n** \**${moment(heg.createdTimestamp).fromNow()}**` ,true) 
-   .addField('انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('`D/M/YYYY HH:mm`')} \n \**${moment(h.joinedAt).fromNow()}\**`, true)
-   .addField('عدد الدعوات', `${inviteCount}`, true) /// Dark last
-   .setFooter(message.author.username , message.author.avatarURL)
-   message.channel.sendEmbed(idDark);
-   })}
-   });
+    if (message.content.startsWith(".bans")) {
+        message.guild.fetchBans()
+        .then(bans => message.channel.send(`${bans.size} عدد اشخاص المبندة من السيرفر `))
+  .catch(console.error);
+}
+});
+
+
+
 
 
 
