4e4a2775528dfb2291a749bb2684c80ed03a332c
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-07 15:41:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -531,15 +531,15 @@ client.on("message", message => {
   const embed = new Discord.RichEmbed() 
       .setColor("#000000")
       .setDescription(`
-${prefix}play ⇏ لتشغيل أغنية برآبط أو بأسم
-${prefix}skip ⇏ لتجآوز الأغنية الحآلية
-${prefix}pause ⇏ إيقآف الأغنية مؤقتا
-${prefix}resume ⇏ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
-${prefix}vol ⇏ لتغيير درجة الصوت 100 - 0
-${prefix}stop ⇏ لإخرآج البوت من الروم
-${prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
-${prefix}queue ⇏ لمعرفة قآئمة التشغيل
-${prefix}viphelp ⇏ لمعرفه اوامر vip
++{prefix}play ⇏ لتشغيل أغنية برآبط أو بأسم
++{prefix}skip ⇏ لتجآوز الأغنية الحآلية
++{prefix}pause ⇏ إيقآف الأغنية مؤقتا
++{prefix}resume ⇏ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
++{prefix}vol ⇏ لتغيير درجة الصوت 100 - 0
++{prefix}stop ⇏ لإخرآج البوت من الروم
++{prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
++{prefix}queue ⇏ لمعرفة قآئمة التشغيل
++{prefix}viphelp ⇏ لمعرفه اوامر vip
 `);
    message.channel.sendEmbed(embed);
     
