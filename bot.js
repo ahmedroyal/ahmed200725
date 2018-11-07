5ee9a86267845312264f9262a5db7d7b38e8d406
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-07 15:45:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -266,6 +266,23 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {       
+if (message.content.startsWith('+clear')) { //xRGRx .. By Julian
+    if(!message.channel.guild) return message.reply('⛔ | This Command For Servers Only!'); 
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | You dont have **MANAGE_MESSAGES** Permission!');
+        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | I dont have **MANAGE_MESSAGES** Permission!');
+ let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 99) return message.reply("**🛑 || يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
+    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
+  }
+  });
+  
+
+
+
 
 
 
