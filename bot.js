8bd7cad11d43c313690c6b739799900209e4d719
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-07 15:48:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -291,4 +291,24 @@ if (message.content.startsWith('+clear')) { //xRGRx .. By Julian
 
 
 
+ client.on('message', message => {
+    if (message.content.startsWith("+avatar")) {
+if(!message.channel.guild) return;
+        var mentionned = message.mentions.users.first();
+    var client;
+      if(mentionned){
+          var client = mentionned; } else {
+          var client = message.author;
+      }
+        const embed = new Discord.RichEmbed()
+                           .addField('Requested by:', "<@" + message.author.id + ">")
+        .setColor(000000)
+        .setImage(`${client.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
