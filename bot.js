3a8e44743908c7c2956bea29c37b60a3d91f4614
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-08 17:06:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -400,51 +400,7 @@ message.channel.send('**تم الارسال في الخاص**');
 
 
 
-  client.on('message', message => { 
-  if (message.content.startsWith(prefix + 'help')) { /// And This is The Channel One Send The Help In Channel // Code By BlueBot Codes.
-      let pages = [`
-+bot
-+server
-+bc <messge>
-+clear <number>
-+inv
-+new اي شئ
-     `]
-   let page = 1;
-  
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setFooter(`Page ${page} of ${pages.length}`)
-      .setDescription(pages[page-1])
-  
-      message.channel.sendEmbed(embed).then(msg => { 
-  
-          msg.react('◀').then( r => {
-              msg.react('▶')
-  
-             setTimeout(() => {
-          msg.delete
-      }, 60 * 1000)
-  
-          const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
-          const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
-  
-  
-          const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
-          const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
-  
-  
-  
-          backwards.on('collect', r => { 
-              if (page === 1) return;
-              page--;
-              embed.setDescription(pages[page-1]);
-              embed.setFooter(`Page ${page} of ${pages.length}`);
-              msg.edit(embed)
-          })
-          forwards.on('collect', r => {
-              if (page === pages.length) return;
-              page++;
+ 
     
 
 
