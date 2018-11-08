07233f4b35986f3cf90a8ec894aadfd4c03bc7de
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-08 18:26:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -426,28 +426,6 @@ client.on("message", msg => {
 
 
 
-
-
-
-
- client.on('message', message => {
-    if(message.content == '+member') {
-    const embed = new Discord.RichEmbed()
-    .setDescription(`**Members info🔋
-:green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
-:heart:dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
-:yellow_heart: idle:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}   
-:black_heart: offline:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size} 
-:blue_heart:   all:  ${message.guild.memberCount}**`)         
-         message.channel.send({embed});
-
-    }
-  });
-
-
-
-
- 
     
 
 
