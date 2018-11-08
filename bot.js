ebe25c9e4e13fdf1d32de6c751ffdc8dad01f0b1
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-08 16:37:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -363,4 +363,20 @@ client.on("guildMemberAdd", member => {
 
 
 
+
+
+client.on('message', message => {
+   if (message.content === "+id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+
+
+
+
 client.login(process.env.BOT_TOKEN);
