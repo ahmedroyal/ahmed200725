4f0226127c77d468fd4022b54f91ff64a5bff0b9
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-09 16:23:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1145,6 +1145,23 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
 
 
 
+
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 10) {
+        m.ban();
+    };
+});
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+};
+ 
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+};
+
+
+
     
 
 client.login(process.env.BOT_TOKEN);
