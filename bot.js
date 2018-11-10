e967cec380d9852071e50056e1dcf831c1aab0a8
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-10 16:03:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`100 Soon`,"http://twitch.tv/S-F")
+client.user.setGame(`+help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
