b875213002edaa87f00ea70fda44fd0f9f0bbed2
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-11 18:29:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1221,19 +1221,6 @@ client.on("message", message => {
 
 
 
-  console.log(`Logged in as ${client.user.tag}!`);
-  });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-
-
-
-
 
 
 
