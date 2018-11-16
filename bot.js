173de9b1d61029452b055e4f09dd7d0b4fe36a9b
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-16 12:56:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,6 +177,26 @@ client.on('message',async message => {
 
 
 
+const moment = reqiure("moment");
+client.on("message", msg => {
+var prefix = "$"//البرفكس
+    if (msg.content.startsWtih(prefix + "id")) {
+let embed = new Discord.RichEmbed()
+     .setColor("RANDOM")
+     .setAuthor(msg.author.username, msg.author.avatarURL)
+     .setTitle('👥Your Information👥')
+     .addField("📜Name + Tag📜", msg.author.tag, true)
+     .setThumbnail(msg.author.avatarURL)
+     .addField('Your ID', msg.author.id, true)
+     .addField('📆Account Created At📆',${moment(msg.author.createdAt).format('D/M/YYYY h:mm a')} **\n** \${moment(msg.author.createdAt).fromNow()}``, true)
+     .setFooter(msg.guild.name, msg.guild.iconURL, true)
+     msg.channel.sendEmbed(embed);
+    }
+  });
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
