665fc1aaef4e60dbbbadf427f5ebc7ab6c2c71fc
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-16 21:27:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1859,7 +1859,7 @@ if (message.content.startsWith(prefix + 'mypoints')) {
     
  
 client.on("message", function(message) 
-   if(message.content.startsWith(prefix + "rps")) {
+   
     let messageArgs = message.content.split(" ").slice(1).join(" ");
     let messageRPS = message.content.split(" ").slice(2).join(" ");
     let arrayRPS = ['**# - Rock**','**# - Paper**','**# - Scissors**'];
