b4787ef402a972e2ad045193d9c4f6739d4a39c2
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-16 14:19:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -484,4 +484,22 @@ client.on('message', message => {
 
 
 
+
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); By RG | OBX ^*RDX#5029
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(${argresult}\n ${m});
+})
+ message.channel.send(\${message.guild.members.filter(m => m.presence.status !== 'all').size}` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
