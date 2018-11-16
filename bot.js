35e560058f0324122fb48bf4e000a68c085bd195
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-16 14:26:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -493,7 +493,7 @@ client.on("message", message => {
   let args = message.content.split(" ").slice(1);
  
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(${argresult}\n ${m});
+
 })
  message.channel.send(\${message.guild.members.filter(m => m.presence.status !== 'all').size}` : عدد الاعضاء المستلمين`); 
  message.delete(); 
