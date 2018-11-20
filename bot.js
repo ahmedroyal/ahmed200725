4f0f5d093dc292014528c4438ef19c5b9b2d60d8
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-20 10:30:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1760,7 +1760,7 @@ client.on('message',async message => {
   if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
   if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
   message.channel.send('✅| **تم عمل الروم بنجاح**');
-  message.guild.createChannel(`Member Count: [ ${msg.guild.memberCount ]` , 'voice').then(c => {
+ 
     console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
     c.overwritePermissions(message.guild.id, {
       CONNECT: false,
