5c3cd96b041192660e27a0239a78ff4613e8e795
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-22 21:17:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1584,7 +1584,7 @@ client.on('message', message => {
 
 
 client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('515236108339118080');
+  const channel = client.channels.get('515244456681930756');
   const size = channel.name.match(/\[\s(\d+)\s\]/);
   if (!size) return channel.setName(`Voice Online: [ ${currentSize} ]`);
   if (currentSize !== size) channel.setName(`Voice Online: [ ${currentSize} ]`);
