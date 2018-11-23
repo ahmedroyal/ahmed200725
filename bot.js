20f5a7e0de3bffca2518c0679d0f14e19bcf189f
ModyA7medway <44779127+ModyA7medway@users.noreply.github.com>
2018-11-23 20:45:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1621,4 +1621,31 @@ if(message.content === '$voice') { // الامر
 
 
 
+
+
+
+
+        var prefix = "$";
+    client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "spam") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR**');
+ 
+ 
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
